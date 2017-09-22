38e49155ddffd4211852da18628c61cdb723d980
Dustin Pluta <dspluta@gmail.com>
2017-09-22 14:16:38 -0700

    Add files via upload

---
+++ wlln.R
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+#
+## Weak Law of Large Numbers Example
+### Brian Vegetabile - 2016 UCI Statsboot Camp
+#
+#-------------------------------------------------------------------------------
+
+sample.sizes <- 10:5000
+n.sims <- length(sample.sizes)
+results <- rep(0, n.sims)
+for(i in 1:n.sims){
+  ss <- sample.sizes[i]
+  random.sample <- rbinom(ss, 1, 0.25)
+  # random.sample <- rexp(ss, rate=0.2)
+  sample.mean <- mean(random.sample)
+  results[i] <- sample.mean
+}
+
+plot(sample.sizes, results, type = 'l',
+     xlab='Sample Size',
+     ylab='Sample Mean')
